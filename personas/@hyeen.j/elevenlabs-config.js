c0050662b2136d748fb36eea1100f6ad92c55259
seyminworks <sey.min.works@gmail.com>
2026-01-04 16:30:12 +0900

    Update CONVERSATION_ID in elevenlabs-config.js

--- personas/@hyeen.j/elevenlabs-config.js
+++ personas/@hyeen.j/elevenlabs-config.js
@@ -9,7 +9,7 @@ const ELEVENLABS_CONFIG = {
   BASE_URL: 'https://api.elevenlabs.io/v1',
 
   // 대화 ID (실제 ElevenLabs에서 생성한 대화의 ID로 교체)
-  CONVERSATION_ID: 'conv_2501kanb0dn0fyh8jtpbjfyrxe45', // 추후 실제 ElevenLabs 대화 ID로 교체
+  CONVERSATION_ID: 'conv_0801kdj1c03zfqvbhbvy9h58xhvk', // 2025-12-28 녹음된 대화
 
   // 에이전트 ID (ElevenLabs ConvAI 에이전트)
   AGENT_ID: 'agent_3501kddfbvyvety9e5syrev9378v',
